28589559e751ca4f3a02494a5a61d5115e6719c6
carbonbd <54285688+carbonbd@users.noreply.github.com>
2021-07-06 17:50:30 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '4c30937a5d92adcbe9288467fe5da3b36e865c03')
+API_KEY = environ.get('API_KEY')
 
 bot = Client('gplink bot',
              api_id=API_ID,
@@ -23,16 +23,16 @@ async def start(bot, message):
 
 
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
-async def link_handler(bot, message):
+async def link_handler(bot, message,data):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your [short link]({short_link})\n By:- @longtime09', quote=True)
+        await message.reply(f'Here is your [short link]({data["shortenedUrl"]})\n By:- @longtime09', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
 
-async def get_shortlink(link):
+async def get_shortlink(link,data):
     url = 'https://shrinkearn.com/api?api='
     params = {'api': API_KEY, '$url': link}
 
@@ -40,6 +40,5 @@ async def get_shortlink(link):
         async with session.get(url, params=params, raise_for_status=True) as response:
             data = await response.json()
             return data["shortenedUrl"]
-
 
 bot.run()
